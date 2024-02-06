0cf03bd6023391bdc479e3a59e4c897b54b52a1e
BrianPizz <bpizzimentijr@outlook.com>
2024-02-06 09:33:20 -0800

    seeded database

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { createPost } from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js";
-
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 // middleware
 const __filename = fileURLToPath(import.meta.url);
@@ -56,5 +58,8 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    // Add data
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(error));
